80011877a7285c1f89c85a05dfa722fedd8c11c3
Ricardo Angeli <rangeli93@gmail.com>
2019-03-14 15:34:10 -0500

    Continued following tutorials.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 angular.module('MyApp', ['ngMaterial'])
-  .run(function(){
-    console.log('MyApp is ready!')
+  .config(function($mdThemingProvider){
+    $mdThemingProvider.theme('default')
+      .primaryPalette('brown')
+      .accentPalette('red')
   })
+  // .run(function(){
+  //   console.log('MyApp is ready!')
+  // })
 // .controller('AppCtrl', function($scope) {
 //   $scope.title1 = 'Button';
 //   $scope.title4 = 'Warn';
